0bd0b49e6f66d5ea053d2f394d2aa3f4d29052db
sanjiaomaojl <1026845550@qq.com>
2019-10-28 16:33:21 +0800

    黑马旅游网

--- src/main/java/com/itheima/travel/service/UserService.java
+++ src/main/java/com/itheima/travel/service/UserService.java
@@ -5,6 +5,7 @@ import com.itheima.travel.domain.User;
 import com.itheima.travel.dao.UserDao;
 import com.itheima.travel.util.Md5Utils;
 import com.itheima.travel.util.MyBatisUtils;
+import com.itheima.travel.util.SmsUtils;
 import org.apache.ibatis.session.SqlSession;
 
 public class UserService {
@@ -28,5 +29,45 @@ public class UserService {
         dao.register(user);
         MyBatisUtils.close(sqlSession);
         return new ResultInfo(true);
+    }
+
+    public ResultInfo findByUsername(String username) {
+        SqlSession sqlSession = MyBatisUtils.openSession();
+        UserDao dao = sqlSession.getMapper(UserDao.class);
+        User user = dao.findByUsername(username);
+        MyBatisUtils.close(sqlSession);
+        if (user != null){
+            return new ResultInfo(false,"该用户名已存在");
+        }else {
+            return new ResultInfo(true,"可以注册");
+        }
+    }
+
+    public ResultInfo findByTelephone(String telephone) {
+        SqlSession sqlSession = MyBatisUtils.openSession();
+        UserDao dao = sqlSession.getMapper(UserDao.class);
+        User user = dao.findByTelephone(telephone);
+        MyBatisUtils.close(sqlSession);
+        if (user != null){
+            return new ResultInfo(false,"手机号已被注册");
+        }else {
+            return new ResultInfo(true,"可以注册");
+        }
+    }
+
+    public ResultInfo sendSms(String telephone,String codeSms) {
+        String signName = "BySjm";
+        String templateCode = "SMS_176521278";
+        String code = "{\"code\":\"" + codeSms + "\"}";
+        try {
+            // 4.调用工具类发送短信
+            SmsUtils.sendSms(telephone, signName, templateCode, code);
+            System.out.println("短信验证码：" + codeSms);
+            // 5.返回结果
+            return new ResultInfo(true, "短信发送成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResultInfo(false, "短信发送失败");
+        }
     }
 }
--- src/main/java/com/itheima/travel/web/servlet/UserServlet.java
+++ src/main/java/com/itheima/travel/web/servlet/UserServlet.java
@@ -1,9 +1,11 @@
 package com.itheima.travel.web.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itheima.travel.domain.ResultInfo;
 import com.itheima.travel.domain.User;
 import com.itheima.travel.service.UserService;
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -33,6 +35,41 @@ public class UserServlet extends BaseServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    //判断用户名是否存在
+    protected void findByUsername(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        ResultInfo resultInfo = service.findByUsername(username);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(resultInfo);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+    }
+
+    //判断手机号
+    protected void findByTelephone(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String telephone = request.getParameter("telephone");
+        ResultInfo resultInfo = service.findByTelephone(telephone);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(resultInfo);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+    }
+
+    protected void sendSms(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String telephone = request.getParameter("telephone");
+        String smsCode = RandomStringUtils.randomNumeric(6);
+        ResultInfo resultInfo = service.sendSms(telephone,smsCode);
+        if (resultInfo.getSuccess()) {// 成功
+            request.getSession().setAttribute("smsCode", smsCode);
+        }
+        // 5.将resultInfo转为json
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(resultInfo);
+        // 6.响应到客户端
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
